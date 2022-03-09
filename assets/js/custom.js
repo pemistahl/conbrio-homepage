37b1ab342590756b2943ce07a02146642e727aff
Peter M. Stahl <pemistahl@gmail.com>
2022-03-09 13:31:06 +0100

    Improve navigation menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,29 @@
-const navbar = document.getElementById("navigation");
+const navbar = document.getElementById("cb-navigation");
+const navLinks = document.querySelectorAll(".nav-link");
+const menuToggle = document.getElementById("cb-navbarContent");
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+
+function toggleNavbar() {
+  navLinks.forEach((l) => {
+    l.addEventListener("click", () => {
+      if (!window.matchMedia("(min-width: 576px)").matches) {
+        bsCollapse.toggle();
+      }
+    });
+  });
+}
 
 function positionNavbar() {
   if (window.matchMedia("(min-width: 576px)").matches) {
     navbar.classList.remove("fixed-bottom");
-    navbar.classList.add("sticky-top");
+    navbar.classList.add("navbar-dark", "bg-dark");
+    if (window.matchMedia("(min-height: 800px)").matches) {
+      navbar.classList.add("sticky-top");
+    } else {
+      navbar.classList.remove("sticky-top");
+    }
   } else {
-    navbar.classList.remove("sticky-top");
+    navbar.classList.remove("sticky-top", "navbar-dark", "bg-dark");
     navbar.classList.add("fixed-bottom");
   }
 }
@@ -13,3 +31,4 @@ function positionNavbar() {
 window.addEventListener("resize", positionNavbar);
 
 positionNavbar();
+toggleNavbar();
