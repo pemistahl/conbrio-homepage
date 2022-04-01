d3a2e3aa33593b851f470b0f7fbe49c44490653a
Peter M. Stahl <pemistahl@gmail.com>
2022-04-01 13:29:48 +0200

    Fix hyphenation and anchors

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,18 @@
 const navbar = document.getElementById("cb-navigation");
 const navLinks = document.querySelectorAll("#cb-navbar-nav > .nav-link");
+const anchors = document.querySelectorAll("#ueberuns, #chorleitung, #konzerte, #mitsingen");
 const menuToggle = document.getElementById("cb-navbar-content");
 const menuButton = document.getElementById("cb-menu-button");
 const mainContent = document.getElementById("cb-main-content");
 const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+
+function addNavbarAnchors() {
+  anchors.forEach(anchor => anchor.classList.add("cb-anchor"));
+}
+
+function removeNavbarAnchors() {
+  anchors.forEach(anchor => anchor.classList.remove("cb-anchor"));
+}
 
 function toggleNavbar() {
   mainContent.addEventListener("touchstart", () => bsCollapse.hide());
@@ -23,12 +32,15 @@ function positionNavbar() {
     navbar.classList.add("navbar-dark", "bg-dark");
     if (window.matchMedia("(min-height: 500px)").matches) {
       navbar.classList.add("sticky-top");
+      addNavbarAnchors();
     } else {
       navbar.classList.remove("sticky-top");
+      removeNavbarAnchors();
     }
   } else {
     navbar.classList.remove("sticky-top", "navbar-dark", "bg-dark");
     navbar.classList.add("fixed-bottom");
+    removeNavbarAnchors();
   }
 }
 
