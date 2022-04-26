02fb40ad0cd2ca3a2b229f5b5d4ad39c5f4e8660
Peter M. Stahl <pemistahl@gmail.com>
2022-04-26 12:57:50 +0200

    Add more icons

--- assets/js/customize-navbar.js
+++ assets/js/customize-navbar.js
@@ -1,10 +1,17 @@
 const navbar = document.getElementById("cb-navigation");
 const navLinks = document.querySelectorAll("#cb-navbar-nav > .nav-link");
+const toTopNavLink = document.getElementById("cb-to-top-nav-link");
+const toTopFooterNavLink = document.getElementById("cb-to-top-footer-nav-link");
 const anchors = document.querySelectorAll("#ueberuns, #chorleitung, #konzerte, #mitsingen");
 const menuToggle = document.getElementById("cb-navbar-content");
 const menuButton = document.getElementById("cb-menu-button");
 const mainContent = document.getElementById("cb-main-content");
 const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+const observer = new IntersectionObserver(
+  ([e]) => toTopNavLink.classList.toggle("d-none", e.intersectionRatio >= 1),
+  {rootMargin: '-1px 0px 0px 0px', threshold: [1]}
+);
+observer.observe(navbar);
 
 function addNavbarAnchors() {
   anchors.forEach(anchor => anchor.classList.add("cb-anchor"));
@@ -32,14 +39,17 @@ function positionNavbar() {
     navbar.classList.add("navbar-dark", "bg-dark");
     if (window.matchMedia("(min-height: 500px)").matches) {
       navbar.classList.add("sticky-top");
+      toTopFooterNavLink.classList.add("d-none");
       addNavbarAnchors();
     } else {
       navbar.classList.remove("sticky-top");
+      toTopFooterNavLink.classList.remove("d-none");
       removeNavbarAnchors();
     }
   } else {
     navbar.classList.remove("sticky-top", "navbar-dark", "bg-dark");
     navbar.classList.add("fixed-bottom");
+    toTopFooterNavLink.classList.remove("d-none");
     removeNavbarAnchors();
   }
 }
